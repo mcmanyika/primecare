e3c22036da664c4896637f0044373cc37f4f4a47
Micah <mcmanyika@gmail.com>
2020-07-05 14:20:02 -0500

    imagekit remove

--- joins/models.py
+++ joins/models.py
@@ -1,15 +1,15 @@
 from __future__ import unicode_literals
 from django.conf import settings
-from imagekit.models import ImageSpecField
-from pilkit.processors import ResizeToFill
 from django.db import models
 from django.contrib.auth.models import User
-from django.urls import reverse    
-from django import forms           
+from django.urls import reverse
+from django import forms
 
 # Create your models here.
+
+
 def upload_location(instance, filename):
-    return "%s/%s" %(instance.id, filename)
+    return "%s/%s" % (instance.id, filename)
 
 
 class UserProfile(models.Model):
@@ -19,18 +19,13 @@ class UserProfile(models.Model):
     gender = models.CharField(max_length=10, null=True, blank=True)
     phone = models.CharField(max_length=25, default='', null=True, blank=True)
     avatar = models.ImageField(null=True, blank=True)
-    image_thumbnail = ImageSpecField(source='avatar',
-        processors=[ResizeToFill(350, 200)],
-        format='JPEG',
-        options={'quality': 60})
 
     user = models.IntegerField(default=1, null=True, blank=True)
-    timestamp = models.DateTimeField(auto_now_add = True, auto_now=False)
-    updated = models.DateTimeField(auto_now_add = False, auto_now=True)
+    timestamp = models.DateTimeField(auto_now_add=True, auto_now=False)
+    updated = models.DateTimeField(auto_now_add=False, auto_now=True)
 
     def __unicode__(self):
-        return 'UserProfile {}'.format(self.id) 
-
+        return 'UserProfile {}'.format(self.id)
 
 
 class t_accts(models.Model):
@@ -41,7 +36,8 @@ class t_accts(models.Model):
     gender = models.CharField(max_length=10, null=True, blank=True)
     dob = models.CharField(max_length=15, null=True, blank=True)
     phone = models.CharField(max_length=25, default='', null=True, blank=True)
-    address = models.CharField(max_length=100, default='', null=True, blank=True)
+    address = models.CharField(
+        max_length=100, default='', null=True, blank=True)
     email = models.CharField(max_length=50, default='', null=True, blank=True)
     emergency_contact = models.CharField(max_length=100, null=True, blank=True)
     account_type = models.CharField(max_length=40, null=True, blank=True)
@@ -51,7 +47,8 @@ class t_accts(models.Model):
         return reverse('acct-delete', kwargs={'id': self.id})
 
     def __unicode__(self):
-        return 't_accts {}'.format(self.id)         
+        return 't_accts {}'.format(self.id)
+
 
 class t_employee_attribute(models.Model):
 
@@ -62,7 +59,8 @@ class t_employee_attribute(models.Model):
     user = models.IntegerField(default=1, null=True, blank=True)
 
     def __unicode__(self):
-        return 't_employee_attribute {}'.format(self.id)    
+        return 't_employee_attribute {}'.format(self.id)
+
 
 class t_client_attribute(models.Model):
 
@@ -74,7 +72,7 @@ class t_client_attribute(models.Model):
     user = models.IntegerField(default=1, null=True, blank=True)
 
     def __unicode__(self):
-        return 't_client_attribute {}'.format(self.id)  
+        return 't_client_attribute {}'.format(self.id)
 
 
 class t_evaluation(models.Model):
@@ -84,8 +82,8 @@ class t_evaluation(models.Model):
     evaluated_date = models.DateField()
     status = models.CharField(max_length=10, null=True, blank=True)
     user = models.IntegerField(null=True, blank=True)
-    timestamp = models.DateTimeField(auto_now_add = True, auto_now=False)
-    updated = models.DateTimeField(auto_now_add = False, auto_now=True)
+    timestamp = models.DateTimeField(auto_now_add=True, auto_now=False)
+    updated = models.DateTimeField(auto_now_add=False, auto_now=True)
 
     def __unicode__(self):
-        return 't_visit_tracker {}'.format(self.id)        
+        return 't_visit_tracker {}'.format(self.id)
