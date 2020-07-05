56a489caffcb3873fc71cbac6163fa3637988c87
Micah <mcmanyika@gmail.com>
2020-07-05 14:21:33 -0500

    libs remove imagekit

--- libs/models.py
+++ libs/models.py
@@ -1,7 +1,5 @@
 from __future__ import unicode_literals
 from django.conf import settings
-from imagekit.models import ImageSpecField
-from pilkit.processors import ResizeToFill
 from django.db import models
 from django.contrib.auth.models import User
 from django.urls import reverse
