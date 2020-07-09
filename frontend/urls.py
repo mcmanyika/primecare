46dc26033a44908f6f5622e3578ab91c48f10d24
Micah <mcmanyika@gmail.com>
2020-07-09 09:42:05 -0500

    restored joins app

--- frontend/urls.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from django.conf.urls import include, url
-from frontend.views import *
-
-urlpatterns = [
-    url(r'^', site_index, name='site-index'),
-    
-]
--- snippets/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from snippets.views import SnippetListView, SnippetDetailView
-
-
-app_name = 'snippets'
-urlpatterns = [
-	path('', SnippetListView.as_view(), name='list'),
-	path('<int:pk>/', SnippetDetailView.as_view(), name='detail')
-]
