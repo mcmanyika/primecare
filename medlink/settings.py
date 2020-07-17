a44fb301d386da442a8dcd24e8eb7d616ac4f465
Micah <mcmanyika@gmail.com>
2020-07-17 15:58:16 -0500

    change login page viewing

--- medlink/settings.py
+++ medlink/settings.py
@@ -86,8 +86,12 @@ WSGI_APPLICATION = 'medlink.wsgi.application'
 if DEBUG:
     DATABASES = {
         'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': 'primecare',
+            'USER': 'site_admin',
+            'PASSWORD': 'focus@1',
+            'HOST': 'localhost',
+            'PORT': '',
         }
     }
 else:
